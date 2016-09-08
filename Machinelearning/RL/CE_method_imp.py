17c68d42785df5fc2df5f8a35806dd2a76412ec1
silverjoda <heizen.sh@gmail.com>
2016-09-08 20:54:02 +0200

    Work on CE_implementation, still WIP.

--- Machinelearning/RL/CE_method_imp.py
+++ Machinelearning/RL/CE_method_imp.py
@@ -8,51 +8,97 @@ import math
 # Make the environment and reset
 env = gym.make('CartPole-v0')
 
+n_steps_per_episode = 100
+batchsize = 10
+
+class NN_controler:
+    def __init__(self, architecture):
+        self.weight_shapes = [(i, j) for i, j in
+                         zip(architecture[:-1], architecture[1:])]
+
+        self.n_layers = len(self.weight_shapes)
+
+        # Initial weight mean and std
+        self.init_weight_mean = 0
+        self.init_weight_std = 1
+
+        # Declare initial weights
+        self.weight_means = self.init_weights()
+        self.weight_stds = self.init_weights()
+
+        # This list will contain lists of all the active weights
+        self.weight_instances = []
+
+    def init_weights(self):
+        return [(np.random.rand(*shape) + self.init_weight_mean) /
+                self.init_weight_std for shape in self.weight_shapes]
+
+    def create_n_weight_instances(self, n):
+
+        # Loop over n instances
+        for i in range(n):
+
+            # List of weights for one instance of the network weights
+            weight_list = []
+
+            for j in range(self.n_layers):
+                weight = np.random.randn(*self.weight_means[j].shape,
+                                         dtype=np.float32)
+
+                # Subtract means and divide by std
+                weight -= self.weight_means[j]
+                weight /= self.weight_stds[j]
+
+                weight_list.append(weight)
+
+            self.weight_instances.append(weight_list)
+
+    def select_n_best_weights(self):
+        pass
+
+    def nn_forward_pass(self):
+        pass
+
+    def fit_new_gaussian(self):
+        pass
+
+    def clear_weight_instances(self):
+        del self.weight_instances[:]
 
 
-def draw_theta_samples():
-    pass
+def simulate_episode(nn_controller):
 
-def simulate_episode(n_steps):
+    # Reset simulation
     observation = env.reset()
-    for t in range(n_steps):
-        env.render()
-        action = env.action_space.sample()
-        observation, reward, done, info = env.step(action)
 
-        if done:
-            print("Episode finished after {} timesteps".format(t+1))
-            break
+    # Create weight instances from the means and std
+    nn_controller.create_n_weight_instances()
 
-def init_weights(shapes, mean, std):
-    return [(np.random.rand(*shape) + mean) / std for shape in shapes]
+    for n in range(batchsize):
+        for t in range(n_steps_per_episode):
+            env.render()
+            action = env.action_space.sample()
+            observation, reward, done, info = env.step(action)
+
+            if done:
+                print("Episode finished after {} timesteps".format(t+1))
+                break
+
+
 
 def main():
-
-    # Amount of samples from the gaussian distribution
-    n_samples_per_update = 10
 
     # Amount of training episodes
     n_episodes = 1000
 
-    # Amount of steps of the simulation per episode
-    n_steps_per_episode = 100
-
     # Neural network architecture (nodes in layers)
     nn_architecture = (4, 8, 4, 1)
 
-    # Amount of weight matrices in the network
-    n_layers = len(nn_architecture) - 1
-
-    weight_shapes = [(i, j) for i, j in
-                     zip(nn_architecture[:-1], nn_architecture[1:])]
-
-    # Declare initial weights
-    weight_means = init_weights
-    weight_std = []
+    # Make the controller network for the task
+    nn_controller = NN_controler(nn_architecture)
 
     for i_episode in range(n_episodes):
-        simulate_episode(n_steps_per_episode)
+        simulate_episode(nn_controller)
 
 
 
