0ae8ea570f30aee7a2ed533657618ae8945f787a
silverjoda <heizen.sh@gmail.com>
2017-05-17 17:40:18 +0200

    Adde

---
+++ Control/main.py
@@ -0,0 +1,31 @@
+
+
+def main():
+
+    # Timestep
+    T = 1/100
+
+    # Make system matrices
+    A = []
+    B = []
+    C = []
+    D = []
+
+    # Make system model from matrices
+
+    # Weight matrices
+    Q = []
+    R = []
+
+
+    # Solve LQR
+
+
+    # Simulate
+
+
+
+    pass
+
+if __name__ == "__main__":
+    main()
