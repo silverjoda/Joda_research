92384181af067176aced382b92480bf5e11f2a47
silverjoda <heizen.sh@gmail.com>
2017-05-26 10:49:53 +0200

    Com main

--- GameTheory/K_SEM/main.py
+++ GameTheory/K_SEM/main.py
@@ -3,11 +3,17 @@ from itertools import *
 import numpy as np
 
 class Node:
-    def __init__(self, history, depth, cur_acts, predecessor,
+    def __init__(self, history, prev_action, depth, cur_acts, predecessor,
                  isleaf=False, value = None):
 
+        # Descendants of the node
+        self.descendants = []
+
         # Copy and append history
-        self.history = deepcopy(history)
+        self.history = deepcopy(history + prev_action)
+
+        # Last action
+        self.prev_action = prev_action
 
         # Get currently available actions
         self.available_acts = cur_acts
@@ -25,6 +31,13 @@ class Node:
         self.NE = None
 
     def getNE(self):
+        if self.isleaf:
+            return self._calculateNE()
+        else:
+            for n in self.descendants:
+                pass
+
+    def _calculateNE(self):
         pass
 
 class NFG:
@@ -91,7 +104,7 @@ class Game:
                     continue
 
                 # Make new node
-                new_node = Node(history + [a], cur_depth + 1,
+                new_node = Node(history, a, cur_depth + 1,
                                    self._get_available_actions(cur_depth + 1),
                                    cur_node,
                                    isleaf = True)
@@ -99,11 +112,14 @@ class Game:
                 # Add node to list
                 self.nodeList.append(new_node)
 
+                # Add node,action pair to descendants
+                cur_node.descendants.append(new_node)
+
             # Make current node the one which continues the game
             c_act = (self.agreed_upon_sequence[cur_depth], self.agreed_upon_sequence[cur_depth])
             history.append(c_act)
 
-            new_node = Node(history + [c_act], cur_depth + 1,
+            new_node = Node(history, c_act, cur_depth + 1,
                             self._get_available_actions(cur_depth + 1),
                             cur_node,
                             isleaf=False)
@@ -111,7 +127,7 @@ class Game:
             cur_node = new_node
 
             if cur_depth == len(self.agreed_upon_sequence) - 1:
-                break
+               break
 
             cur_depth += 1
 
