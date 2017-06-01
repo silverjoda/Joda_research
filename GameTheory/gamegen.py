447081c8ff3c3b20cee28377703553066585afae
shagas <azayev@spaceknow.com>
2017-06-01 16:37:50 +0200

    Added game

--- GameTheory/gamegen.py
+++ GameTheory/gamegen.py
@@ -1,98 +1,118 @@
 import numpy as np
 import gurobipy as g
 
-def gengame():
+#np.random.seed(1337)
+np.set_printoptions(precision=3, suppress=True)
 
-    # Gurobi model
-    m = g.Model()
-
-    a = {}
-    b = {}
-    cce = {}
-    ne = {}
-
-    for i in range(2):
-        for j in range(2):
-            a[i, j] = m.addVar(lb=-10, ub=10, vtype=g.GRB.CONTINUOUS)
-            b[i, j] = m.addVar(lb=-10, ub=10, vtype=g.GRB.CONTINUOUS)
-            cce[i, j] = m.addVar(lb=-10, ub=10, vtype=g.GRB.CONTINUOUS)
-
-    ne['P1', 'A'] = m.addVar(lb=-10, ub=10, vtype=g.GRB.CONTINUOUS)
-    ne['P1', 'B'] = m.addVar(lb=-10, ub=10, vtype=g.GRB.CONTINUOUS)
-
-    ne['P1', 'A'] = m.addVar(lb=-10, ub=10, vtype=g.GRB.CONTINUOUS)
-    ne['P2', 'B'] = m.addVar(lb=-10, ub=10, vtype=g.GRB.CONTINUOUS)
+def genRandGame(shape=(3,3)):
+    return np.random.rand(-10, 11, (2,)+shape)
+    # return np.random.rand(2, shape[0], shape[1]) * 10 - 5
 
 
-def genRandGame():
-    return np.random.randint(-10, 11, size=(2,2,2)).astype(np.float32)
+def noreg(game):
 
-def noreg(game, iters):
+    w1 = np.ones(game.shape[-2])
+    w2 = np.ones(game.shape[-1])
 
-    w1 = np.array([1.,1.])
-    w2 = np.array([1.,1.])
+    u1 = np.zeros(game.shape[-2])
+    u2 = np.zeros(game.shape[-1])
 
-    state_counts = np.zeros((2,2))
+    states = []
 
     eps = .1
 
-    for _ in range(iters):
+    for i in range(10000):
 
-        a1 = np.random.choice(2, p=w1/np.sum(w1))
-        a2 = np.random.choice(2, p=w2/np.sum(w2))
+        p1 = w1/np.sum(w1)
+        p2 = w2/np.sum(w2)
 
-        state_counts[a1, a2] += 1
+        a1 = np.random.choice(len(w1), p=p1)
+        a2 = np.random.choice(len(w2), p=p2)
 
-        w1 = w1*(1-eps)**game[0, :, a2]
-        w2 = w2*(1-eps)**game[0, a1, :]
+        states.append((a1, a2))
+        u1 += game[0, :, a2]
+        u2 += game[1, a1, :]
 
-    return state_counts / float(iters)
+        w1 = (1-eps)**-norm_weights(u1)
+        w2 = (1-eps)**-norm_weights(u2)
 
-def checkNE(u, noreg_cce, eps):
+    return state_distribution(states, game.shape[1:])
 
-    # Get marginal probabilities from cce
-    p1_sup = np.sum(noreg_cce, 1)
-    p2_sup = np.sum(noreg_cce, 0)
 
-    # Calculate nash equillibrium from game
-    pa = (u[1, 1, 1] - u[1, 1, 0]) / (u[1, 0, 0] - u[1, 1, 0] - u[1, 0, 1] + u[1, 1, 1])
-    pc = (u[0, 1, 1] - u[0, 0, 1]) / (u[0, 0, 0] - u[0, 0, 1] - u[0, 1, 0] + u[0, 1, 1])
+def norm_weights(u):
+    w = np.zeros_like(u)
+    med_ix = np.argsort(u)[len(u)//2]
 
-    # Make NE strategy
-    ne_p1_sup = np.array([pa, 1 - pa])
-    ne_p2_sup = np.array([pc, 1 - pc])
+    for i in range(len(u)):
+        w[i] = u[i] - u[med_ix]
+    return np.clip(w, -100, 100)
 
-    return np.linalg.norm(p1_sup - ne_p1_sup, 2) < eps and np.linalg.norm(p2_sup - ne_p2_sup, 2) < eps
+
+def state_distribution(states, game_shape):
+    sigma = np.zeros(game_shape)
+    for s in states:
+        sigma[s] += 1
+    return sigma / len(states)
+
+
+def better_than_ne(game, sigma, eps=0.01):
+    p1 = np.sum(sigma, axis=1)
+    p2 = np.sum(sigma, axis=0)
+
+    exp_u1 = np.sum(sigma*game[0])
+    exp_u2 = np.sum(sigma*game[1])
+
+    br_u1 = np.max(np.sum(game[0]*p2, axis=1))
+    br_u2 = np.max(np.sum(game[1].T*p1, axis=1))
+
+    print 'CCE expected value=[{:.3f}, {:.3f}]\n' \
+          'best response=     [{:.3f}, {:.3f}]'.format(exp_u1, exp_u2, br_u1, br_u2)
+
+    return br_u1 - exp_u1 < -eps and br_u2 - exp_u2 < -eps, p1, p2
 
 
 def main():
 
-    eps = 0.001
-    iters = 0
+    # chicken_dare_game = np.array([[[0,7],[2,6]],[[0,2],[7,6]]])
+    # chicken_dare_ce = np.array([[0, 1./3], [1./3, 1./3]])
+    #
+    # print check_ne(chicken_dare_game, chicken_dare_ce)
 
     while True:
+        # Generate random game with different nash and CCE
+        game = genRandGame(shape=(4,4))
 
-        iters += 1
+        # coordination game
+        # game = np.array([[[3, 0, 0], [0, 2, 0], [0, 0, 1]],
+        #                  [[3, 0, 0], [0, 2, 0], [0, 0, 1]]])
 
-        # Generate random game
-        game = genRandGame()
+        # shapley game
+        # game = np.array([[[1, 0, 0], [0, 1, 0], [0, 0, 1]],
+        #                  [[0, 1, 0], [0, 0, 1], [1, 0, 0]]])
 
-        # Get cce from noreg dynamics
-        noreg_cce = noreg(game, 1000)
 
-        # Check whether game nash is same as found cce by no regret dynamics
-        eps = 0.01
-        res = checkNE(game, noreg_cce, eps)
+        # Get state count from noregret game
+        sigma = noreg(game)
 
-        if not res:
-            print "Found game with NRD converging to CCE after {} iters.".format(iters)
-            print "Game matrices a: {}".format(game)
+        # Check whether sequence of states is NE
+        res, p1, p2 = better_than_ne(game, sigma)
+
+        if np.max(sigma) > 0.99:
+            print 'pure'
+            # TODO: fix pure
+        # else:
+        #     print 'not pure:', p1, p2
+
+        if res:
+            print "Found game with NRD converging to CCE!=NE."
+            print "Game matrices: \n{}".format(game)
+            print "sigma: \n{}".format(sigma)
+            print "p1={}\np2={}".format(p1, p2)
             break
 
-
+        print '---------------------'
 
 
 if __name__ == "__main__":
     main()
-
 
