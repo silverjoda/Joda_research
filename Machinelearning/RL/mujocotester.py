23ddc3cb531fc622317c8aef5d564706e1104627
silverjoda <heizen.sh@gmail.com>
2017-06-26 16:21:04 +0200

    Done

---
+++ Machinelearning/RL/mujocotester.py
@@ -0,0 +1,6 @@
+from mujoco_py import *
+
+
+
+
+
--- Machinelearning/RL/open_ai_gym_tester.py
+++ Machinelearning/RL/open_ai_gym_tester.py
@@ -3,11 +3,11 @@ import gym
 import math
 from gym.spaces import Discrete, Box
 
-env = gym.make("Pong-v0")
+env = gym.make("InvertedPendulum-v1")
 
 for i_episode in range(20):
     observation = env.reset()
-    for t in range(100):
+    for t in range(1000):
         env.render()
         action = env.action_space.sample()
         observation, reward, done, info = env.step(action)
