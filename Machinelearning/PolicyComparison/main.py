1e89fb5be863cdbc66f1b58578f3183d7d1222ef
silverjoda <heizen.sh@gmail.com>
2017-07-18 20:05:20 +0200

    Added policy comparison

--- Machinelearning/PolicyComparison/main.py
+++ Machinelearning/PolicyComparison/main.py
@@ -2,6 +2,35 @@ import tflearn as tfl
 
 def main():
 
+    #  1) Train reactive policy on chosen environment
+
+    # Make simulation
+
+
+    # Make policy
+
+
+    # Train policy
+
+
+    # Evaluate and visualize policy
+
+    #  2) Train recurrent policy on chosen environment
+
+    # Make simulation
+
+
+    # Make policy
+
+
+    # Train policy
+
+
+    # Evaluate and visualize policy
+
+    # 3) Perform temporal analysis on recurrent policy
+
+
     print 'Done. '
 
 
