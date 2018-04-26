b6dbf46c2debe6d12eb658b1e600dbcce8a19355
silverjoda <silverjoda>
2018-04-26 15:19:40 +0200

    Continue on networks

--- Advansense/audio_writer.py
+++ Advansense/audio_writer.py
@@ -1,4 +1,6 @@
+import numpy as np
 from scipy.io import wavfile
+import matplotlib.pyplot as plt
 import os
 
 class Audiowriter:
@@ -6,7 +8,17 @@ class Audiowriter:
         self.path = 'audio_results'
 
     def writetofile(self, data, fname, rate):
+        data = data / np.max(data)
+        data = data * 2 - np.max(data)
+        print data.shape
+        plt.plot(data[:44111])
+        plt.show()
+        print "Writing data type: {}, min: {}, max: {}".format(data.dtype, np.min(data), np.max(data))
         wavfile.write(os.path.join(self.path, fname), rate, data)
 
     def writetostream(self, data):
         raise NotImplementedError
+
+
+#data = np.random.randn(20000)
+#wavfile.write("shitfile.wav", 44100,data)
--- Advansense/main.py
+++ Advansense/main.py
@@ -1,9 +1,12 @@
 from video_dataset import VidSet
 from network import VizEncoder
 from audio_writer import Audiowriter
+import numpy as np
 import cv2
 
 def main():
+
+    LOAD = False
 
     imres = 224
     samplerate = 44100
@@ -16,23 +19,28 @@ def main():
     # Make dataset
     video_reader = VidSet(imres)
 
-    # Train
-    for i in range(n_iters):
-        batch = video_reader.get_rnd_sample(batchsize)
-        mse = network.train(batch)
+    if LOAD:
+        # Load weights
+        network.restore_weights()
+    else:
+        # Train
+        for i in range(n_iters):
+            batch = video_reader.get_rnd_sample(batchsize)
+            mse = network.train(batch)
 
-        if i % 10 == 0:
-            print "Iteration {}/{}, mse: {}".\
-                format(i, n_iters, mse)
+            if i % 10 == 0:
+                print "Iteration {}/{}, mse: {}".\
+                    format(i, n_iters, mse)
 
-    # Save weights
-    network.save_weights()
+        # Save weights
+        network.save_weights()
 
     # Test
     test_sample = video_reader.get_cons_sample(300)
     encoded_test_sample = network.encode(test_sample)
+    concatenated_result = np.reshape(encoded_test_sample, [-1])
     audio_writer = Audiowriter()
-    audio_writer.writetofile(encoded_test_sample,
+    audio_writer.writetofile(concatenated_result,
                              "firsttest.wav",
                              samplerate)
 
